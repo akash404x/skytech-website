ca465521ffd5dbc6de727746718cfb73312ca521
unknown <aakashsingh10570012@gmail.com>
2026-05-17 17:30:01 +0530

    removed mongodb

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,12 +1,9 @@
 'use client';
 
 import { AuthProvider } from '@/contexts/AuthContext';
-import { SessionProvider } from 'next-auth/react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
-      <AuthProvider>{children}</AuthProvider>
-    </SessionProvider>
+    <AuthProvider>{children}</AuthProvider>
   );
 }
